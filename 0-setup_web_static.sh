521730ba731b229bd334b4a4abcbf898755ae3d3
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-08 15:39:11 -0500

    adding nginx configuratio

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,4 +12,19 @@ sudo tee /data/web_static/releases/test/index.html <<EOF
   </body>
 </html>
 EOF
+if [ -e /data/web_static/current ];then
+    rm /data/web_static/current
+fi
 sudo ln -s /data/web_static/releases/test /data/web_static/current
+sudo chown ubuntu:ubuntu /data/
+sudo tee /data/web_static/current > /dev/null <<EOF                                    
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}
+EOF
+/etc/init.d/nginx restart
