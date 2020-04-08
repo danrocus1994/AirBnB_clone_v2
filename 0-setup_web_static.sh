e6457306db3b4005bdb3d64dada62513cd7f62a6
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-08 15:13:17 -0500

    adding symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,4 +2,14 @@
 # Setup Web servers with Nginx Airbnb Deploy
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/{releases,shared,releases/test}
+sudo mkdir -p /data/web_static/{shared,releases/test}
+sudo tee /data/web_static/releases/test/index.html <<EOF
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+sudo ln /data/web_static/releases/test/ /data/web_static/current
