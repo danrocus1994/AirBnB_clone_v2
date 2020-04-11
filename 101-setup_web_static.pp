b3cf7b0b59d5bc56ecf1f95de2ba5a13d578ab87
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-11 12:25:40 -0500

    lint corrections

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,29 +3,23 @@
 exec{'update':
     command => '/usr/bin/apt-get -y update',
 }
-->
-package{['nginx',
-         'nginx-common',
-	 'nginx-full',]:
+-> package{['nginx', 'nginx-common', 'nginx-full',]:
     ensure  => 'installed',
     require => Exec['update']
 }
-->
-file{['/data',
+-> file{['/data',
       '/data/web_static',]:
     ensure  => directory,
     owner   => 'ubuntu',
     group   => 'ubuntu',
     recurse => true,
 }
-->
-file{['/data/web_static/releases',
+-> file{['/data/web_static/releases',
       '/data/web_static/shared',
       '/data/web_static/releases/test',]:
     ensure  => directory,
 }
-->
-file{'/data/web_static/releases/test/index.html':
+-> file{'/data/web_static/releases/test/index.html':
     content => "<html>
   <head>
   </head>
@@ -34,13 +28,11 @@ file{'/data/web_static/releases/test/index.html':
   </body>
 </html>",
 }
-->
-file{'/data/web_static/current':
-    ensure  => link,
-    target  => '/data/web_static/releases/test/',
+-> file{'/data/web_static/current':
+    ensure => link,
+    target => '/data/web_static/releases/test/',
 }
-->
-file_line{'hbnb_static':
+-> file_line{'hbnb_static':
     ensure             => present,
     path               => '/etc/nginx/sites-available/default',
     after              => 'server_name localhost;',
@@ -49,7 +41,6 @@ file_line{'hbnb_static':
     multiple           => false,
     line               => 'location /hbnb_static {alias /data/web_static/current/;}',
 }
-->
-service{'nginx':
+-> service{'nginx':
     ensure  => running,
 }
