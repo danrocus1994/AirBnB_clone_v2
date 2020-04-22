40845ee9b422d6e39427e951abe97be2ef7eaaed
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-22 22:29:04 +0000

    flask application to serve states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This module inits a Flask application server
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage(uknown):
+    """
+    apply a close to the storage engine to reload
+    """
+    print(uknown, type(uknown))
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    List all state in db
+    """
+    states = storage.all(State)
+    #print(dir(states.items()))
+    #print(dir(states[0]))
+    #comm = input(">>> ")
+    resp = {}
+    
+    for state_id, state in states.items():
+        print(state.name)
+        resp[state_id.split('.')[1]] = state.name
+    states = []
+    for key in sorted(resp, key=resp.get, reverse=False):
+        states.append((key, resp[key]))
+    
+    
+    #    print(eval("state."+comm))
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    Routing the main path
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
