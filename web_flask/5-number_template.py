3823bfba04d119cda9bd68e20458f02ea070ea8b
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-22 12:08:40 -0500

    5 number html template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This module inits a Flask application server
+"""
+
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__, root_path='')
+
+
+@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
+def number_template_route(n):
+    return render_template("5-number.html", number="Number: {}".format(n))
+
+
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
+def number_route(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/python', methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def python_route(text="is_cool"):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c_route(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    Routing the main path
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
